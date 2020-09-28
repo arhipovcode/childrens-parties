0b0bd8d7548166ac0103f4bde871c0148befc521
Arkhipov Mikhail <arhipovma@yandex.ru>
2020-09-28 22:48:42 +0500

    768 готов

--- script/range.js
+++ script/range.js
@@ -5,3 +5,10 @@ $(".js-range-slider").ionRangeSlider({
   from: 0,
   to: 100,
 });
+$(".js-range-slider_two").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 20000,
+  from: 0,
+  to: 1000,
+});
