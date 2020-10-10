511ad11f069d47a101920081bafbe55e6619fe32
Arkhipov Mikhail <arhipovma@yandex.ru>
2020-10-10 17:56:02 +0500

    объединил файлы css, минимизировал css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css')
 var jsmin = require('gulp-jsmin');
+var concatCss = require('gulp-concat-css');
 var rename = require('gulp-rename');
 
 function buildJs() {
@@ -17,6 +19,18 @@ gulp.task('js-min', function () {
       .pipe(jsmin())
       .pipe(rename({suffix: '.min'}))
       .pipe(gulp.dest('dist'));
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('css/style.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('main-css', function () {
+  return gulp.src('dist/css/*.css')
+    .pipe(concatCss("styles/style.css"))
+    .pipe(gulp.dest('dist'));
 });
  
 gulp.watch('./src/*.js', buildJs);
